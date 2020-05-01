b15e517772e12e870e23329d04f02e480d910cd6
KrupaKalola <krupak@nividata.com>
2020-05-01 18:00:54 +0530

    index file updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
-// import App from "./Container/App";
-import WeatherComponent from './weatherComponent'
-ReactDOM.render(<WeatherComponent />, document.getElementById("root")); 
+import App from "./Container/App";
+ReactDOM.render(<App />, document.getElementById("root")); 
